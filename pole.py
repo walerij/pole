aa899d76f1766d1879ca82f609fb82ec18081d41
Zhdanov Valery <becwal@yandex.ru>
2022-11-14 23:52:55 +0300

    цикл игры - длина слова + з попытки. уточнение алгоритма проверки букв

--- pole.py
+++ pole.py
@@ -1,11 +1,18 @@
 from game import game
 print("Здравствуйте, друзья! В эфире программа 'Поле чудес'")
 
-quest = game().quest
-answer = game().answer
-print("Загаданное слово ",game().answer)
+game=game()
+quest = game.quest
+slovo = len(quest)+3 #создание цикла - длина слова + 3 попытки
 
-a=input ("Введите Вашу букву:")
+answer = game.answer
+print("Загаданное слово ",answer)
 
-print(game().check_answer(a))
-print(game().answer)
+for i in range(0,slovo):
+    a=input ("Введите Вашу букву:")
+
+    print(game.check_answer(a))
+    answer = game.answer
+    print(answer)
+
+print("Загаданное слово:",quest)    
