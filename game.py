8b26f4bfc661b4a7ec5b225e73cb6256981fb28b
Zhdanov Valery <becwal@yandex.ru>
2022-11-14 22:28:51 +0300

    Cоздание переменной answer - в которой будет отгадываться слово и вывод его (звездочки)

--- game.py
+++ game.py
@@ -1,8 +1,15 @@
 import random
 class game:
     def __init__(self) -> None:
-        self.worklist=[]
+        self.worklist=[] #создаем словарь - пустой список
         with open("words.txt",'r') as f:
-            self.workslist=f.read().splitlines()                   
-        self.quest=self.workslist[random.randint(0,len(self.workslist))]
-        self.win=""        
+            self.workslist=f.read().splitlines()         #заполняем его из файла words          
+        self.quest=self.workslist[random.randint(0,len(self.workslist))]#вопрос - выбор случайный
+        self.answer = ""
+        self.set_answer()
+        #self.set_answer()
+        self.win=""  
+
+    def set_answer(self):
+        for i in self.quest:
+            self.answer+="*"          
--- pole.py
+++ pole.py
@@ -1,5 +1,5 @@
 from game import game
 print("Здравствуйте, друзья! В эфире программа 'Поле чудес'")
 
-g = game()
-print("Загаданное слово ",g.quest)
+quest = game().quest
+print("Загаданное слово ",game().answer)
