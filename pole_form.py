c4f8a418c7d2cacd7eec148e0213cabdf9e29f71
Zhdanov Valery <becwal@yandex.ru>
2022-11-18 00:01:50 +0300

    Добавление формы для игры winforms

--- pole_form.py
+++ pole_form.py
@@ -1,2 +1,13 @@
-import tkinter
+from tkinter import *
 
+
+form = Tk()
+form.geometry("1000x700")
+form.title("Поле чудес")
+
+fr_word = Frame(form)
+fr_word.grid(row=0, column=1)
+la_word= Label(fr_word, bg="orange", width=70, height=2,text="слово")
+la_word.pack(side=LEFT)
+
+form.mainloop()
