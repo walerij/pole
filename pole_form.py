8a92cb4ebc7b062eb28f6629522d76c1c9144fc0
Zhdanov Valery <becwal@yandex.ru>
2022-11-26 13:08:48 +0300

    Подключение класса игры и поиск слова по буквам

--- pole_form.py
+++ pole_form.py
@@ -1,18 +1,28 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
+
+from game import game #импортируем класс game
 
 
 form = Tk()
 form.geometry("700x500")
 form.title("Поле чудес")
-
+# создаем фреймы
 fr_word = Frame(form)
 fr_word.grid(row=0, column=1)
 fr_button = Frame(form)
 #fr_button.grid(row=1, column=1)
 fr_pole = Frame(form)
 fr_pole.grid(row=2, column=1)
-la_word= Label(fr_word, bg="green", fg="white",font="Arial,42", width=100, height=2,text="слово")
+
+#создаем игру
+game=game()
+quest = game.quest
+global answer 
+answer= game.answer
+
+#прогружаем слово и играем
+la_word= Label(fr_word, bg="green", fg="white",font="Arial,42", width=100, height=2,text=answer)
 la_word.pack(side=LEFT)
 def click_button():
     la_word["text"]=btn.cget('text')
@@ -23,10 +33,17 @@ for r in range(3):
 #la_button= Label(fr_button, bg="green", width=100, height=20,text="буквы")
 #la_button.pack(side=LEFT)
 
-pole = Entry(fr_pole,fg="yellow", bg="blue", width=50, font="Tahoma,16")
+pole = Entry(fr_pole,fg="yellow", bg="blue", width=50,text="a", font="Tahoma,16")
 pole.pack()
 def pole_check():
-    la_word["text"]=pole.get()
+    letter=pole.get()
+    if game.check_answer(letter)==0:
+        messagebox.showinfo(title="Информация", message="Буквы "+letter+" нет в этом слове!")
+    elif game.check_answer(letter)==1:
+        messagebox.showinfo(title="Информация", message="Буква "+letter+"! Есть такая буква в этом слове!")    
+    answer = game.answer        
+    la_word["text"]=answer
+
 pole_button=Button(fr_pole,text="Назвать букву",command=pole_check)
 pole_button.pack()
 
